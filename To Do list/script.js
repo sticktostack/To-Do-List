f38ccff0b2a5bcf8bd07f0b511fbe6cc78a8d8c0
sticktostack <rajdeep6153@gmail.com>
2026-03-05 19:25:26 +0530

    Final Changes

--- To Do list/script.js
+++ To Do list/script.js
@@ -1,30 +1,71 @@
-let inputBox = document.getElementById('textarea')
-let add = document.getElementById('add')
-let myList = document.getElementById('myList')
-add.addEventListener('click',function(){
-  if(inputBox.value === ''){
-    alert('You Must Write Something')
-  }else{
-    let li = document.createElement('li')
-    li.innerHTML =inputBox.value
-    myList.appendChild(li)
-    inputBox.value = ''
-    let cross = document.createElement('cross')
-    cross.id='cross'
-    cross.innerHTML = "\u00D7"
-    li.appendChild(cross)
-  }
-})
-myList.addEventListener('click',(event)=>{
-  if(event.target.tagName === "LI"){
-      event.target.classList.toggle('checked')
-  }else if(event.target.id === "cross"){
-    event.target.parentElement.remove()
-  }
-})
+let inputBox = document.getElementById("textarea");
+let add = document.getElementById("add");
+let myList = document.getElementById("myList");
 
-let body = document.querySelector('body')
-body.addEventListener('keypress',function(e){
-  if(e==Space)
-  console.log('e');
-})
+function addTask(){
+
+if(inputBox.value === ""){
+alert("You must write something");
+return;
+}
+let li = document.createElement("li");
+li.innerHTML = inputBox.value;
+let editBtn = document.createElement("span");
+editBtn.innerHTML = "✏️";
+editBtn.className = "edit";
+let cross = document.createElement("span");
+cross.innerHTML = "✖";
+cross.className = "delete";
+
+li.appendChild(editBtn);
+li.appendChild(cross);
+myList.appendChild(li)
+inputBox.value = "";
+
+saveData();
+}
+
+add.addEventListener("click",addTask);
+
+inputBox.addEventListener("keypress",function(e){
+if(e.key === "Enter"){
+addTask();
+}
+});
+
+myList.addEventListener("click",function(event){
+if(event.target.tagName === "LI"){
+event.target.classList.toggle("checked");
+saveData();
+}
+
+else if(event.target.classList.contains("delete")){
+event.target.parentElement.remove();
+saveData();
+}
+
+else if(event.target.classList.contains("edit")){
+
+let li = event.target.parentElement;
+let currentText = li.firstChild.textContent;
+
+let newTask = prompt("Edit your task:",currentText);
+
+if(newTask !== null && newTask !== ""){
+li.firstChild.textContent = newTask;
+saveData();
+}
+}
+
+});
+
+/* LOCAL STORAGE */
+function saveData(){
+localStorage.setItem("tasks",myList.innerHTML);
+}
+
+function showTask(){
+myList.innerHTML = localStorage.getItem("tasks");
+}
+
+showTask();
